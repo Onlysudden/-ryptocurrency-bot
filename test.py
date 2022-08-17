78a7ab18bd6e26a44ca428bb60accbc100ca19a9
Onlysudden <trofimof09@mail.ru>
2022-08-17 20:30:11 +0300

    Add get_ticker

--- test.py
+++ test.py
@@ -8,8 +8,20 @@ def get_info():
     
     return response.text
 
+def get_ticker(coin1="btc", coin2="usd"):
+    #response = requests.get(url="https://yobit.net/api/3/ticker/eth_btc-xrp_btc")
+    response = requests.get(url=f"https://yobit.net/api/3/ticker/{coin1}_{coin2}?ignore_invalid=1")
+
+
+    with open("ticker.txt", "w") as file:
+        file.write(response.text)
+    
+    return response.text
+
+
 def main():
-    print(get_info())
+    #print(get_info())
+    print(get_ticker())
 
 if __name__ == "__main__":
     main()
